592c184f5ac3e5491ced1cbbccd121c21a4875e5
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-05 22:48:01 +0700

    Update slug.js

--- utils/slug.js
+++ utils/slug.js
@@ -1,1 +1,19 @@
-adada
+// utils/slug.js
+'use strict';
+/**
+ * Generate a random slug string
+ * @param {number} length - length of the slug (default 8)
+ * @returns {string} random slug
+ */
+function generateSlug(length = 8) {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let slug = '';
+  for (let i = 0; i < length; i++) {
+    slug += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return slug;
+}
+
+module.exports = {
+  generateSlug,
+};
