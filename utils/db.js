4f85d4932cf4fb3dca93b674215c471dd7d68876
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-05 23:14:08 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,55 @@
+const sqlite3 = require('sqlite3').verbose();
+if (err) {
+console.error('Failed to connect to SQLite database:', err.message);
+process.exit(1);
+}
+console.log('Connected to SQLite database.');
+});
+return db;
+}
+
+
+function ensureSchema() {
+const db = connect();
+db.serialize(() => {
+db.run(`CREATE TABLE IF NOT EXISTS files (
+id TEXT PRIMARY KEY,
+original_name TEXT,
+mime TEXT,
+size INTEGER,
+storage_key TEXT,
+url TEXT,
+created_at TEXT DEFAULT (datetime('now'))
+)`);
+});
+}
+
+
+function insertFile({ id, original_name, mime, size, storage_key, url }) {
+const db = connect();
+return new Promise((resolve, reject) => {
+db.run(
+`INSERT INTO files (id, original_name, mime, size, storage_key, url) VALUES (?, ?, ?, ?, ?, ?)`,
+[id, original_name, mime, size, storage_key, url],
+function (err) {
+if (err) return reject(err);
+resolve();
+}
+);
+});
+}
+
+
+function getFile(id) {
+const db = connect();
+return new Promise((resolve, reject) => {
+db.get(`SELECT * FROM files WHERE id = ?`, [id], (err, row) => {
+if (err) return reject(err);
+resolve(row);
+});
+});
+}
+
+
+module.exports = { connect, ensureSchema, insertFile, getFile };
 
