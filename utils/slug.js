22141c81f707c64dc434354b2075f3a9e08c46f2
Rafiza-Fried-Chicken <mangvios0@gmail.com>
2025-09-05 23:14:26 +0700

    Update slug.js

--- utils/slug.js
+++ utils/slug.js
@@ -1,1 +1,8 @@
+function makeSlug(length = 10) {
+const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let out = '';
+for (let i = 0; i < length; i++) out += alphabet[Math.floor(Math.random() * alphabet.length)];
+return out;
+}
+module.exports = { makeSlug };
 
